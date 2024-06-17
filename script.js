2122ba305d6bfde1fd66df57a7a3702d19c492ec
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-06-18 01:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	// let output1 = document.getElementByName('fname');
+	// let output2 = document.getElementByName('lname');
+	let first = input[0].value;
+	let last = input[1].value;
+	alert(first + last);
 
 }
